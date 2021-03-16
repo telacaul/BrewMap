0018a6c0b23d035c696d2d934545cd12a733e123
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-03-15 18:41:22 -0700

    added first API link

--- assets/js/brewAPI-Rich.js
+++ assets/js/brewAPI-Rich.js
@@ -1,10 +1,12 @@
 
-function findBrewery(zipCode) {
+function findBrewery(city) {
 
-    let breweryAPI = "https://api.openbrewerydb.org/breweries?by_postal=" + zipCode;
+    let breweryAPI = "https://api.openbrewerydb.org/breweries?by_city=" + city;
 
     fetch(breweryAPI).then(response => response.json()).then(data => console.log(data));
     // console.log to get API info
 
-    fetch(breweryAPI).then(response => response.json()).then(function)
+    fetch(breweryAPI).then(response => response.json()).then(function(data) {
+        
+    });
 }
